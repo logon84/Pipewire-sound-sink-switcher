46504b0656137a31dd5b8424ae37fe6aefc92bc4
logon84 <logon84@users.noreply.github.com>
2024-07-30 00:10:05 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# Pipewire-sound-sink-switcher
+BASH script to switch pipewire sinks. Configure it to launch with a keyboard shortcut to quickly switch between sinks.
+
+
+
+[![Donate](https://www.paypalobjects.com/es_ES/ES/i/btn/btn_donateCC_LG.gif)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=ER2LTNM5LZDTY)  
+BTC address: 12cQuFn7yMSfDB1uKPGKLMQ7XSj1XF2sVA
---
+++ sound_sink_switcher.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#Sinks array space-separated. Add or remove sink names from the outputs of this command:
+#pw-dump | jq '.[] | select(.info.props."media.class"=="Audio/Sink") | .info.props."node.name"'
+declare -a SINKS_TO_SWITCH=("alsa_output.pci-0000_00_03.0.hdmi-stereo" "alsa_output.pci-0000_00_1b.0.analog-stereo")
+SINK_ELEMENTS=$(echo ${#SINKS_TO_SWITCH[@]})
+
+#get current sink
+ACTIVE_SINK_ID=$(wpctl status | sed -n '/Audio/,${p;/Sink endpoints/q}' | grep '*' | awk '{print ($3+0)}')
+ACTIVE_SINK_NAME=$(pw-dump | jq '.[] | select(.id=='"$ACTIVE_SINK_ID"') | .info.props."node.name"' | tr -d \")
+ACTIVE_ARRAY_INDEX=$(echo ${SINKS_TO_SWITCH[@]/$ACTIVE_SINK_NAME//} | cut -d/ -f1 | wc -w | tr -d ' ')
+
+#get next ID in array
+NEXT_ARRAY_INDEX=$((($ACTIVE_ARRAY_INDEX+1)%$SINK_ELEMENTS))
+NEXT_SINK_NAME=${SINKS_TO_SWITCH[$NEXT_ARRAY_INDEX]}
+NEXT_SINK_ID=$(pw-dump | jq '.[] | select(.info.props."node.name"=="'"$NEXT_SINK_NAME"'") | .id')
+
+#switch to sink
+wpctl set-default $NEXT_SINK_ID
+notify-send AudioSwitch "Switching to $NEXT_SINK_ID : $NEXT_SINK_NAME"
